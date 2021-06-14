cf8a5321c5e564b92b6c5d52870d4cb156f51c67
twinkle-shark <80078652+twinkle-shark@users.noreply.github.com>
2021-06-14 11:08:23 +0800

    Add files via upload

---
+++ Proxy/Client.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Client {
+
+    public static void main(String[] args) {
+	// write your code here
+        CLocalPicShow cls=new CLocalPicShow();
+        cls.ShowPic("photo");
+    }
+}
---
+++ Proxy/IShowPic.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface IShowPic {
+    public void ShowPic(String picname);
+}
