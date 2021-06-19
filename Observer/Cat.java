eebb163a8d7b0d691768fc926fd43e8136c4bc27
twinkle-shark <80078652+twinkle-shark@users.noreply.github.com>
2021-06-19 17:32:38 +0800

    Add files via upload

---
+++ Observer/Cat.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Cat extends MySubject {
+    @Override
+    public void cry() {
+        System.out.println("猫叫");
+        System.out.println("--------------");
+        for(Object obs:observers){
+            ((MyObserver)obs).response();
+        }
+    }
+}
---
+++ Observer/Dog.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Dog implements MyObserver {
+    @Override
+    public void response() {
+        System.out.println("狗跟着叫");
+    }
+}
